0f1248f403b222f50d4f828c18dfe0d3f77edef6
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-02-01 22:31:04 +0100

    Create aVeryBigSum.js

---
+++ aVeryBigSum.js
@@ -0,0 +1,13 @@
+function aVeryBigSum(ar) {
+    // Write your code here
+  let resultOfSum = 0;
+
+  for (let i = 0; i < ar[i]; i++) {
+      resultOfSum += ar[i];
+  }
+  console.log(resultOfSum);
+  return resultOfSum;
+}
+
+aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
+//5000000015
